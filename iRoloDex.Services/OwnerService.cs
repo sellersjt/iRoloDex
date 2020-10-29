eb3663920a7d1eba91787853753963cabc0122b2
Darron <darronfrancis@gmail.com>
2020-10-28 21:22:01 -0400

    firstCommit

---
+++ iRoloDex.Services/OwnerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iRoloDex.Services
+{
+    public class OwnerService
+    {
+        private readonly Guid _userId;
+
+        public OwnerService(Guid userId)
+        {
+            _userId = userId;
+        }
+    }
+}
