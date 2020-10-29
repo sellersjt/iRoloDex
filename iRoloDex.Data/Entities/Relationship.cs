df72b6a0e0ce933aa06c6318cf260cd1b8450f6a
Alex <alexgolding@icloud.com>
2020-10-28 21:19:22 -0400

    RelationshipPush1

---
+++ iRoloDex.Data/Entities/Relationship.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iRoloDex.Data.Entities
+{
+    public class Relationship
+    {
+        [Key]
+        public int RelationshipId { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
